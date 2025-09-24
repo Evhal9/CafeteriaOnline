5822a45202a555f4b91f7ab53a8f09c416da775b
Mely Salerno <melysalernoflores@gmail.com>
2025-09-24 13:05:36 -0300

    Agregando detalles al logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,22 @@
-import carrito from "../assets/carrito.svg"
+import carrito from '../assets/carrito.svg'
 import './Header.css'
+
 function Header(){
     return (
         <>
         <nav id="nav" >
 
-                <section id="logo-container"> <img id="logo"src="/lunaIcon.svg" alt="LogoLuna" /></section>
+                <section id="logo-container"> 
+                    <img id="logo"src="/lunaIcon.svg" alt="LogoLuna" />
+                    <h1 id="marca">Luna & Granos cafe </h1>
+                    </section>
                 <section id="nav-items">
-                    <a class="a-nav redir"href="#">Inicio</a>
-                    <a class="a-nav redir" href="">Carta</a>
-                    <a class="a-nav redir" href="">Contacto</a>
-                    <a class="a-nav redir" href="">Nosotros</a>
-                    <a class="a-nav" href=""><img id="carrito" src={carrito} alt="CarritoCompra" /></a>
-                </section>
+                    <a class="a-nav "href="#">Inicio</a>
+                    <a class="a-nav " href="">Carta</a>
+                    <a class="a-nav " href="">Contacto</a>
+                    <a class="a-nav" href=""> Nosotros</a>
+                    <a class="a-nav"  href=""> <img id="carrito" src={carrito} alt="" /></a>  
+                                    </section>
         </nav>
         </>
     )
