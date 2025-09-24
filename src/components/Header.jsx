84b0c3ac44a9fd45d6b1ece2ecaffb2ad0cbcffc
Mely Salerno <melysalernoflores@gmail.com>
2025-09-24 08:51:22 -0300

    Se realizo el cambio de los colores, altura y posicion de los elementos del nav, se corrigio problemas visuales

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,16 +4,15 @@ function Header(){
     return (
         <>
         <nav id="nav" >
-            <div id="nav-container">
+
                 <section id="logo-container"> <img id="logo"src="/lunaIcon.svg" alt="LogoLuna" /></section>
                 <section id="nav-items">
-                    <a class="a-nav"href="#">Inicio</a>
-                    <a class="a-nav" href="">Carta</a>
-                    <a class="a-nav" href="">Contacto</a>
-                    <a class="a-nav" href="">Nosotros</a>
+                    <a class="a-nav redir"href="#">Inicio</a>
+                    <a class="a-nav redir" href="">Carta</a>
+                    <a class="a-nav redir" href="">Contacto</a>
+                    <a class="a-nav redir" href="">Nosotros</a>
                     <a class="a-nav" href=""><img id="carrito" src={carrito} alt="CarritoCompra" /></a>
                 </section>
-            </div>
         </nav>
         </>
     )
