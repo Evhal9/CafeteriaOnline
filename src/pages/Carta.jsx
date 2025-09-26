c52d069078c6f83f6cab7a2f539056f06c822ab6
Sadino07 <sdiaznoia@gmail.com>
2025-09-26 18:40:40 -0300

    Agregar imagenes y Carta.css

--- src/pages/Carta.jsx
+++ src/pages/Carta.jsx
@@ -1,9 +1,11 @@
+import './Carta.css';
+
 function Carta() {
     const productos = [
-        { id: 1, nombre: "Café Espresso", precio: 2500, categoria: "bebidas-calientes",descripcion: "Café negro intenso y aromático",imagen: "/cafe-espresso.jpg"  },
-        { id: 2, nombre: "Capuccino", precio: 3000, categoria: "bebidas-calientes" ,descripcion: "Café con sabor chocolate",imagen: "/capuccino.jpg" },
-        { id: 3, nombre: "Medialuna", precio: 3500, categoria: "comidas",descripcion: "Medialuna de manteca o de grasa",imagen: "/medialuna.jpg" },
-        { id: 4, nombre: "Tostado apto vegano", precio: 3500, categoria: "comidas",descripcion: "Tostado de miga de J&Q apto vegano",imagen: "/tostado.jpg" }
+        { id: 1, nombre: "Café Espresso", precio: 2500, categoria: "bebidas-calientes",descripcion: "Café negro intenso y aromático",imagen: "../../public/productos/cafe-espresso.jpg"  },
+        { id: 2, nombre: "Capuccino", precio: 3000, categoria: "bebidas-calientes" ,descripcion: "Café con sabor chocolate",imagen: "../public/productos/capuccino.jpg" },
+        { id: 3, nombre: "Medialuna", precio: 3500, categoria: "comidas",descripcion: "Medialuna de manteca o de grasa",imagen: "/public/productos/medialuna.jpg" },
+        { id: 4, nombre: "Tostado apto vegano", precio: 3500, categoria: "comidas",descripcion: "Tostado de miga de J&Q apto vegano",imagen: "/productos/tostado.jpg" }
     ];
 
     const productosPorCategoria = productos.reduce((acc, producto) => {
@@ -40,14 +42,13 @@ function Carta() {
                     {productosCategoria.map(producto => (
                             <div key={producto.id} className="producto-card">
                             <div className="producto-imagen">
-                                {/*alguna img*/}
-                                <div className="imagen-placeholder">
-                                    {producto.nombre.charAt(0)}
-                                </div>
-                                </div>
+                                
+                                <img src={producto.imagen} alt={producto.imagen} className="producto-img" />
+                                
+                            </div>
                             <div className="producto-infoo">
                                 <h3 className="producto-nombre">{producto.nombre}</h3>
-                                <p className="producto-descripcion">${producto.descripcion}</p>
+                                <p className="producto-descripcion">{producto.descripcion}</p>
                                 <div className="producto-precio-container">
                                     <span className="producto-precio">${producto.precio}</span>
                                     <button className="Agregar-al-carrito">Agregar +</button>
