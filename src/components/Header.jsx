09dcb67a0a6666a028f2acee9c168c7d3cecc6ee
Mely Salerno <melysalernoflores@gmail.com>
2025-09-27 09:35:18 -0300

    arreglando rutas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,11 +10,11 @@ function Header(){
                     <p>Date tu gusto ☕</p>
                 </section>
                 <section id="logo-container"> 
-                    <Link id="logo" to="/Inicio"><img src="/lunaIcon.svg" alt="LogoLuna" /></Link>
+                    <Link id="logo" to="/"><img src="/lunaIcon.svg" alt="LogoLuna" /></Link>
                     <h1 id="marca">Luna & Granos cafe </h1>
                     </section>
                 <section id="nav-items">
-                    <Link className="a-nav" to="/Inicio">Inicio</Link>
+                    <Link className="a-nav" to="/">Inicio</Link>
                     <Link className="a-nav" to="/Carta">Carta</Link>
                     <Link className="a-nav" to="/Contacto">Contacto</Link>
                     <Link className="a-nav" to="/Nosotros"> Nosotros</Link>
